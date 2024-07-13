5e1ebe16aecf3ca4173672f293d9af0e36086cc9
Chorong Lee <mrarongsya@gmail.com>
2024-07-14 00:18:59 +0900

    update

--- wiw/server.js
+++ wiw/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+//초기 셋팅 코드 ===========================================================
+//=======================================================================
+
 app.listen(8080, () => {
   console.log("http://localhost:8080 에서 서버 실행중");
 });
@@ -11,3 +14,12 @@ app.use(express.static(path.join(__dirname, "wiw-react/build")));
 app.get("/", function (요청, 응답) {
   응답.sendFile(path.join(__dirname, "/wiw-react/build/index.html"));
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//==================================================================================================
+//==================================================================================================
+//==================================================================================================
+
+app.get("/login", function (요청, 응답) {});
